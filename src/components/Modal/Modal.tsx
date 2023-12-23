3c7dbe54083fde2ef55d4a11ee8d08a5462e7cd2
Amir Fayzullin <far20022002@mail.ru>
2023-12-23 14:53:55 +0500

    feat: added outside modal click handling

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,19 +1,24 @@
-import React, {PropsWithChildren} from 'react';
-
-import {Close} from "../icons";
+import React, {PropsWithChildren, useRef} from 'react';
 import {createPortal} from "react-dom";
+import { useOnClickOutside } from 'usehooks-ts';
+
+import {Close} from "components/icons";
 
 import * as Styles from './styles';
 
 interface ModalProps {
   isOpen: boolean;
-  onClose?: () => void;
+  onClose: () => void;
 }
 
 export const Modal = ({children, onClose, isOpen}: PropsWithChildren<ModalProps>) => {
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(ref, onClose);
+
   return isOpen ? createPortal((
     <Styles.Wrapper>
-      <Styles.Modal>
+      <Styles.Modal ref={ref}>
         <Close onClick={onClose} />
         <Styles.Content>
           {children}
