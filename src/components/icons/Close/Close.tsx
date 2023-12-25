e2ffa8b615dea083e762bf810641f0aed525631a
Amir Fayzullin <far20022002@mail.ru>
2023-12-25 21:14:17 +0500

    feat: changed icons

--- src/components/icons/Close/Close.tsx
+++ src/components/icons/Close/Close.tsx
@@ -1,12 +1,13 @@
 import React, {ComponentPropsWithoutRef} from 'react';
 
-export const Close = ({width = 24, height = 24, color = '#000', ...restProps}: ComponentPropsWithoutRef<'svg'>) => {
+export const Close = ({width = 24, height = 24, color = 'currentColor', ...restProps}: ComponentPropsWithoutRef<'svg'>) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={width} height={height} viewBox="0 0 50 50" {...restProps}>
-      <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"
-            fill={color}
-      >
-      </path>
-    </svg>
+  <svg xmlns="http://www.w3.org/2000/svg" height={height} width={width} viewBox="0 0 384 512" {...restProps}>
+    <path
+      opacity="1"
+      fill={color}
+      d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+  />
+  </svg>
   );
 };
