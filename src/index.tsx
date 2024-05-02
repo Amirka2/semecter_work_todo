762766842f881641b16096cfa4af378ac9d6a4f2
Amir <far20022002213@gmail.com>
2024-05-02 12:08:59 +0500

    feat: added custom webpack-config, tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,5 +15,3 @@ root.render(
       <App />
     </QueryClientProvider>
 );
-
-reportWebVitals();
